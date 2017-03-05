5aa22f4e9c68a6a37a0da4d03f13dd982ad4cebe
ryanbas21 <ryanbas21@gmail.com>
2017-03-05 17:13:51 -0500

    saving HomeContainer styles fix, import was bad

--- duckr/app/containers/home/HomeContainer.js
+++ duckr/app/containers/home/HomeContainer.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react';
-import { Home } from 'components'
-import { container, title, slogan } from './styles.css'
-
+import { Home } from 'components';
 
 class HomeContainer extends Component {
-  render() {
-    return (
-      <div className={container}>
-        <p className={title}> {`Duckr`}</p>
-        <p className={slogan}>{`The real time, cloud based, modular, scalable, growth hack, social platform. In the cloud`}</p>
-      </div>
-    )
-  }
+  render () {
+      return (
+        <Home />
+      )
+    }
 }
 
 
