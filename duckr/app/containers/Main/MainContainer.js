08a56d3dcdce7560bfeb8fb654103b459e567e27
ryanbas21 <ryanbas21@gmail.com>
2017-03-05 09:09:14 -0500

    saving the final files from app

---
+++ duckr/app/containers/Main/MainContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class MainContainer extends Component {
+  render() {
+    return (
+      <p>{'Hello World!'}</p>
+    );
+  }
+}
+
+export default MainContainer;
