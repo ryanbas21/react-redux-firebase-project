454b0a51f8a437d0373100f5dd75d6088aff4a50
ryanbas21 <ryanbas21@gmail.com>
2017-03-05 17:13:21 -0500

    saving Navigation refactor

--- duckr/app/components/Navigation/Navigation.js
+++ duckr/app/components/Navigation/Navigation.js
@@ -1,37 +1,38 @@
-import React from 'react';
+import React, { PropTypes } from 'react'
 import { Link } from 'react-router'
 import { container, navContainer, link } from './styles.css'
 
+Navigation.propTypes = ActionLinks.propTypes = NavLinks.propTypes = {
+  isAuthed: PropTypes.bool.isRequired,
+}
+
 function NavLinks ({isAuthed}) {
-  return isAuthed === true ?
-  <ul>
-    <li>
-      <Link to='/'>{Home}</Link>
-    </li>
-  </ul>
-  : null
+  return isAuthed === true
+    ? <ul>
+        <li><Link to='/' className={link}>{'Home'}</Link></li>
+      </ul>
+    : <noscript />
 }
 
 function ActionLinks ({isAuthed}) {
-  return isAuthed === true ?
-  <ul>
-    <li>New Duck</li>
-    <li><Link to='/logout'>{`Logout`}</Link></li>
-  </ul> :
-  <ul>
-    <li><Link to='/'>{`Home`}</Link></li>
-    <li><Link to='/auth'>{`Authenticate`}</Link></li>
-  </ul>
+  return isAuthed === true
+    ? <ul>
+        <li>NEW DUCK</li>
+        <li><Link to='/logout' className={link}>{'Logout'}</Link></li>
+      </ul>
+    : <ul>
+        <li><Link to='/' className={link}>{'Home'}</Link></li>
+        <li><Link to='/auth' className={link}>{'Authenticate'}</Link></li>
+      </ul>
 }
 
-export default function Navigation({isAuthed}) {
+export default function Navigation ({isAuthed}) {
   return (
-    <div>
-      <nav>
-        <NavLinks isAuthed={isAuthed}/>
+    <div className={container}>
+      <nav className={navContainer}>
+        <NavLinks isAuthed={isAuthed} />
         <ActionLinks isAuthed={isAuthed} />
       </nav>
-
     </div>
-  );
+  )
 }
