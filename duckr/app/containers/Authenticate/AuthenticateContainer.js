a385989492408bd4700f74a537d2d7dff93d3128
ryanbas21 <ryanbas21@gmail.com>
2017-03-05 15:18:11 -0500

    saving the Authentication Container file for the Authentication component

---
+++ duckr/app/containers/Authenticate/AuthenticateContainer.js
@@ -0,0 +1,16 @@
+import React, {
+  Component
+} from 'react';
+import { Authenticate } from 'components';
+
+
+export default class AuthenticateContainer extends Component {
+  render() {
+    return (
+      <div>
+        <Authenticate />
+      </div>
+    );
+  }
+
+}
